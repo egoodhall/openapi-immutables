788fdfc0125696f5a82ac488fec390a25c92bf65
Eric Marshall <ericmarshall715@gmail.com>
2020-04-05 21:01:51 -0400

    Fix tests

--- src/test/java/io/github/emm035/openapi/immutables/v3/OpenApiTest.java
+++ src/test/java/io/github/emm035/openapi/immutables/v3/OpenApiTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flipkart.zjsonpatch.DiffFlags;
 import com.flipkart.zjsonpatch.JsonDiff;
+import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
 import io.github.emm035.openapi.immutables.v3.jackson.Json;
 import org.junit.Test;
@@ -39,17 +40,28 @@ public class OpenApiTest {
       JsonNode readFromFile = om.readTree(jsonString);
       JsonNode reparsed = om.valueToTree(om.treeToValue(readFromFile, OpenApi.class));
 
-      JsonNode diff = JsonDiff.asJson(readFromFile, reparsed, EnumSet.of(DiffFlags.OMIT_VALUE_ON_REMOVE));
-
-      // Uncomment to log json
-      if (diff.size() != 0) {
-        System.out.println(readFromFile.toString());
-        System.out.println(reparsed.toString());
-      }
-      assertThat(diff).isEmpty();
+      onlyRemovedEmptyNodes(readFromFile, reparsed);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private void onlyRemovedEmptyNodes(JsonNode source, JsonNode target) {
+    JsonNode diff = JsonDiff.asJson(source, target, EnumSet.noneOf(DiffFlags.class));
+
+    boolean onlyRemovedEmptyContent = Streams.stream(diff.iterator())
+      .map(node -> node.get("op").asText().equals("remove")
+        && node.get("value").isContainerNode()
+        && node.get("value").size() == 0)
+      .reduce(true, (l, r) -> l && r);
+
+    // Uncomment to log json
+    if (!onlyRemovedEmptyContent) {
+      System.out.println(source.toString());
+      System.out.println(target.toString());
+      System.out.println(diff);
+    }
+    assertThat(onlyRemovedEmptyContent).isTrue();
   }
 
   private String loadResourceFileAsString(String fileName) {
