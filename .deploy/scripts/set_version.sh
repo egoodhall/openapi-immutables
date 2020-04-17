21bbe039050972efcce480851a198398042536cb
Eric Marshall <ericmarshall715@gmail.com>
2020-04-17 17:47:56 -0400

    Fix versioning

--- .deploy/scripts/set_version.sh
+++ .deploy/scripts/set_version.sh
@@ -5,7 +5,7 @@ if [[ "$#" != "1" ]]; then
 fi
 
 version="$1"
-if [[ "$(echo $version | grep -E '^[0-9]+\.[0-9]+(?:\.[0-9]+|(?:-[a-zA-Z0-9/.-]+)?-SNAPSHOT)$')" == "" ]]; then
+if [[ "$(echo $version | grep -E '^(?:[0-9]+\.[0-9]+\.[0-9]+)|(?:1.0(-.+)*-SNAPSHOT)$')" == "" ]]; then
   echo "Tag must use semver or snapshot versioning (invalid: $version)"
   exit 1
 fi
@@ -13,8 +13,8 @@ fi
 current_version="$(mvn -q --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec -Dexec.executable="echo" -Dexec.args='${project.version}')"
 if [[ "$version" == "$current_version" ]]; then
   echo "Maven version matches tag version. Skipping version bump."
-  exit 1
+  exit
 fi
 
 echo "Version changed. Bumping Maven version to '$version'"
-mvn versions:set -DnewVersion="$version"
+mvn -q --non-recursive versions:set -DnewVersion="$version"
