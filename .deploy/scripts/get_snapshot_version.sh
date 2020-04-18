80893874bbd7b02ee7eefa8e0c2a60f600344f03
Eric Marshall <ericmarshall715@gmail.com>
2020-04-18 13:25:13 -0400

    No branch name for master snapshots

--- .deploy/scripts/get_snapshot_version.sh
+++ .deploy/scripts/get_snapshot_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 branch="${GITHUB_REF#refs/heads/}"
-if [[ "$branch" == "" ]];then
+if [[ "$branch" == "master" ]];then
   echo "1.0-SNAPSHOT"
 else
   echo "1.0-$branch-SNAPSHOT"
